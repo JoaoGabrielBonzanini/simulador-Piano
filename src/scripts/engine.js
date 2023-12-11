2c30aecf4fda92bdd7b9547899e4443ba48c3669
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2023-12-11 12:28:07 -0300

    Volume slider working

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,18 +1,36 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+const volumeSlider = document.querySelector(".volume-slider input");
+let mapedKeys = [];
 let audio = new Audio("src/sounds/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/sounds/${key}.wav`
     audio.play();
+
+    const clikcedKey = document.querySelector(`[data-key ="${key}"]`)
+    clikcedKey.classList.add("active");
+    setTimeout(() => {
+        clikcedKey.classList.remove("active");
+    },150)
 }
 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key)
     key.addEventListener("click", () => playTune(key.dataset.key))
-})
+
+    mapedKeys.push(key.dataset.key);
+});
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key)
-    console.log(e.key);
-})
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key)
+        console.log(e.key);
+    }
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value
+}
+
+volumeSlider.addEventListener("input", handleVolume)
